425b0302ec8fb217d8e8beac313930ad79b1a389
Jonathan Chaves <jonathanchcr@gmail.com>
2020-07-29 18:12:42 -0600

    Agregado de Signin

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Dashboard from './Dashboard';
+import SignIn from './Signin';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode>    
     <Dashboard />
   </React.StrictMode>,
   document.getElementById('root')
